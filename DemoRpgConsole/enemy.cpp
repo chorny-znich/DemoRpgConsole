992e3c7b8d111e657b29ce4a6aa1360171977af1
chorny-znich <vargar.dt@gmail.com>
2024-07-11 14:59:46 +0300

    feat: the enemy moves to a player when he see him

--- DemoRpgConsole/enemy.cpp
+++ DemoRpgConsole/enemy.cpp
@@ -18,21 +18,53 @@ void Enemy::chooseAction()
 
 void Enemy::move()
 {
-  size_t decision = dr::EngineUtility::getRandomInRange(1, 11);
-  if (decision == 1 || decision == 2) {
-    mMovement = { 0, 0 };
+  Location* pHeroLocation{ nullptr };
+  for (auto& loc : mEntityEnvironment) {
+    for (auto& item : loc.second) {
+      if (item->isPlayer()) {
+        pHeroLocation = item;
+        break;
+      }
+    }
   }
-  else if (decision == 3 || decision == 4) {
-    mMovement = { -1, 0 };
+  // The hero is in enemy's line of sight
+  if (pHeroLocation != nullptr) {
+    GameData::Position heroPosition = pHeroLocation->getPosition();
+    if (mPosition.first - heroPosition.first == 0) {
+      if (mPosition.second > heroPosition.second) {
+        mMovement = { 0, -1 };
+      }
+      else {
+        mMovement = { 0, 1 };
+      }  
+    }
+    else {
+      if (mPosition.first > heroPosition.first) {
+        mMovement = { -1, 0 };
+      }
+      else {
+        mMovement = { 1, 0 };
+      }
+    }
   }
-  else if (decision == 5 || decision == 6) {
-    mMovement = { 1, 0 };
-  }
-  else if (decision == 7 || decision == 8) {
-    mMovement = { 0, 1 };
-  }
-  else if (decision == 9 || decision == 10) {
-    mMovement = { 0, -1 };
+  // Enemy moves in random direction
+  else {
+    size_t decision = dr::EngineUtility::getRandomInRange(1, 11);
+    if (decision == 1 || decision == 2) {
+      mMovement = { 0, 0 };
+    }
+    else if (decision == 3 || decision == 4) {
+      mMovement = { -1, 0 };
+    }
+    else if (decision == 5 || decision == 6) {
+      mMovement = { 1, 0 };
+    }
+    else if (decision == 7 || decision == 8) {
+      mMovement = { 0, 1 };
+    }
+    else if (decision == 9 || decision == 10) {
+      mMovement = { 0, -1 };
+    }
   }
 }
 
